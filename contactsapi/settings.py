e074706334c2e3fdb2784a90d7fd9ecd25ebbae6
Renish Charaniya <renish9923@gmail.com>
2021-04-26 10:45:56 +0530

    add application files

--- contactsapi/settings.py
+++ contactsapi/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -134,3 +135,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
